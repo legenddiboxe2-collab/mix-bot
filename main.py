f506dcc56035fed52b47e511b43c5b3f4fb87f88
legenddiboxe2-collab <legenddiboxe2@gmail.com>
2026-05-18 14:34:09 +0545

    Update main.py

--- main.py
+++ main.py
@@ -225,7 +225,7 @@ async def check(ctx, uid: str):
                 f"**• {'Status' if lang == 'en' else 'Statut'} :** "
                 f"{'No sufficient evidence of cheat usage on this account.' if lang == 'en' else 'Aucune preuve suffisante pour confirmer l’utilisation de hacks sur ce compte.'}\n"
                 f"**• {'Nickname' if lang == 'en' else 'Pseudo'} :** `{nickname}`\n"
-                f"**• {'Player ID' if lang == 'en' else 'ID du joueur'} :** `{id_str}`\n"
+                f"**{'Player ID' if lang == 'en' else 'ID du joueur'} :** {player_id}\n"
                 f"**• {'Region' if lang == 'en' else 'Région'} :** `{region}`"
             )
             # embed.set_image(url="https://i.ibb.co/Kx1RYVKZ/notbanned.gif")
